52fc563ea7cdb170779c6ad9306f82fcedb06be2
giangnt21697 <truonggiang21697@gmail.com>
2026-07-21 16:59:28 +0700

    Update prepare.ps1

--- scripts/prepare.ps1
+++ scripts/prepare.ps1
@@ -1,31 +1,11 @@
 $TargetFolder = "C:\It-Support\SCM"
-$SourceFolder = "\\10.2.15.93\Setup\3CX"
 
-Write-Host "===== DOWNLOAD TEST ====="
+Write-Host "===== PREPARE ====="
 
 New-Item -ItemType Directory -Path $TargetFolder -Force | Out-Null
+
 attrib +h $TargetFolder
 
-# Xóa file cũ (không xóa folder)
-Get-ChildItem $TargetFolder -Force -ErrorAction SilentlyContinue |
-    Remove-Item -Force -Recurse
+Write-Host "Target Folder : $TargetFolder"
 
-Write-Host "Copy từ:"
-Write-Host $SourceFolder
-
-Copy-Item `
-    -Path "$SourceFolder\*" `
-    -Destination $TargetFolder `
-    -Recurse `
-    -Force
-
-Write-Host ""
-
-Write-Host "Các file đã copy:"
-
-Get-ChildItem $TargetFolder -Recurse | ForEach-Object{
-    Write-Host $_.FullName
-}
-
-Write-Host ""
-Write-Host "===== DOWNLOAD SUCCESS ====="
+Write-Host "===== PREPARE SUCCESS ====="
