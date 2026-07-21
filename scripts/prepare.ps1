ed773faa636818a870fd23f9d8aff139641c358d
giangnt21697 <truonggiang21697@gmail.com>
2026-07-21 17:54:33 +0700

    Update prepare.ps1

--- scripts/prepare.ps1
+++ scripts/prepare.ps1
@@ -1,11 +1,44 @@
+param(
+    [string]$Software
+)
+
+$ShareFolder  = "\\10.2.15.93\Setup"
 $TargetFolder = "C:\It-Support\SCM"
 
+Write-Host ""
 Write-Host "===== PREPARE ====="
+Write-Host ""
+
+Write-Host "Software : $Software"
 
 New-Item -ItemType Directory -Path $TargetFolder -Force | Out-Null
 
 attrib +h $TargetFolder
 
-Write-Host "Target Folder : $TargetFolder"
+Get-ChildItem $TargetFolder -Force |
+    Remove-Item -Recurse -Force
 
+$Source = Join-Path $ShareFolder $Software
+
+Write-Host ""
+Write-Host "Source : $Source"
+
+if(!(Test-Path $Source))
+{
+    throw "Không tìm thấy thư mục phần mềm: $Source"
+}
+
+Copy-Item `
+    "$Source\*" `
+    $TargetFolder `
+    -Recurse `
+    -Force
+
+Write-Host ""
+Write-Host "Copied Files"
+
+Get-ChildItem $TargetFolder -Recurse |
+    Select Name
+
+Write-Host ""
 Write-Host "===== PREPARE SUCCESS ====="
