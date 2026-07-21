6f673417d4e094aaaa3da3dcc466866fa42bbd71
giangnt21697 <truonggiang21697@gmail.com>
2026-07-21 16:46:31 +0700

    Update prepare.ps1

--- scripts/prepare.ps1
+++ scripts/prepare.ps1
@@ -1,14 +1,30 @@
 $TargetFolder = "C:\It-Support\SCM"
+$ShareFolder  = "\\10.2.15.93\Setup"
 
 Write-Host "===== PREPARE FOLDER ====="
 
-# Luôn tạo lại, nếu đã có thì không báo lỗi
 New-Item -ItemType Directory -Path $TargetFolder -Force | Out-Null
-
-# Đặt thuộc tính Hidden
 attrib +h $TargetFolder
 
-Write-Host "Folder:"
-Resolve-Path $TargetFolder
+Write-Host "Target Folder : $TargetFolder"
 
-Write-Host "Done."
+if(Test-Path $ShareFolder)
+{
+    Write-Host "Share Connected."
+
+    $Software = Get-ChildItem $ShareFolder -Directory
+
+    Write-Host "Total Software : $($Software.Count)"
+
+    $Software |
+        Select-Object -First 5 |
+        ForEach-Object{
+            Write-Host " - $($_.Name)"
+        }
+}
+else
+{
+    throw "Cannot access $ShareFolder"
+}
+
+Write-Host "===== DONE ====="
