b67b3803f81986588475da03e361a88a92e27db4
giangnt21697 <truonggiang21697@gmail.com>
2026-07-22 20:21:44 +0700

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -2,19 +2,6 @@ param(
     [string]$Software
 )
 
-# ============================================================
-# INSTALL.PS1
-#
-# Chức năng:
-# 1. Nhận tên phần mềm từ Jenkins
-# 2. Tìm bộ cài đã được prepare.ps1 copy về
-# 3. Nếu có script riêng -> chạy script riêng
-# 4. Nếu không có -> chạy installer/default.ps1
-#
-# File này KHÔNG chứa logic cài đặt.
-# Nó chỉ đóng vai trò Dispatcher.
-# ============================================================
-
 $TargetFolder = "C:\It-Support\SCM"
 
 Write-Host ""
@@ -22,55 +9,36 @@ Write-Host "===== INSTALL ====="
 Write-Host ""
 Write-Host "Software : $Software"
 
-# ============================================================
-# Tìm bộ cài trong thư mục SCM.
-# Theo thiết kế hiện tại Prepare chỉ copy đúng 1 installer,
-# nên chỉ cần lấy file đầu tiên.
-# ============================================================
+# ------------------------------------------------------------
+# Tìm installer đã được prepare.ps1 copy về
+# ------------------------------------------------------------
 
 $Installer = Get-ChildItem `
     -Path $TargetFolder `
-    -File |
-    Where-Object {
-        $_.Extension -in ".exe", ".msi"
-    } |
+    -File `
+    -Include *.exe, *.msi `
+    -Recurse |
     Select-Object -First 1
 
 if ($null -eq $Installer)
 {
-    throw "Không tìm thấy installer trong $TargetFolder"
+    throw "Không tìm thấy installer."
 }
 
-Write-Host ""
-Write-Host "Found Installer"
-Write-Host "Name      : $($Installer.Name)"
-Write-Host "Extension : $($Installer.Extension)"
-Write-Host "Full Path : $($Installer.FullName)"
-Write-Host ""
+# ------------------------------------------------------------
+# Chọn installer script
+# ------------------------------------------------------------
 
-# ============================================================
-# Kiểm tra xem phần mềm có script riêng hay không.
-#
-# Ví dụ:
-# installer\Android_Studio.ps1
-# installer\Office.ps1
-#
-# Nếu tồn tại:
-#      -> chạy script riêng.
-#
-# Nếu không:
-#      -> chạy default.ps1
-# ============================================================
+$InstallerFolder = Join-Path $PSScriptRoot "installer"
 
-$CustomInstaller = Join-Path `
-    $PSScriptRoot `
-    "installer\$Software.ps1"
+$CustomInstaller = Join-Path $InstallerFolder "$Software.ps1"
+
+$DefaultInstaller = Join-Path $InstallerFolder "default.ps1"
 
 if(Test-Path $CustomInstaller)
 {
-    Write-Host "Using Custom Installer"
-    Write-Host $CustomInstaller
     Write-Host ""
+    Write-Host "Installer Script : $Software.ps1"
 
     & $CustomInstaller `
         -Software $Software `
@@ -78,13 +46,10 @@ if(Test-Path $CustomInstaller)
 }
 else
 {
-    Write-Host "Using Default Installer"
     Write-Host ""
+    Write-Host "Installer Script : default.ps1"
 
-    & "$PSScriptRoot\installer\default.ps1" `
+    & $DefaultInstaller `
         -Software $Software `
         -Installer $Installer
 }
-
-Write-Host ""
-Write-Host "===== INSTALL SUCCESS ====="
